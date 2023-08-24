714dabe7f2c5c23f33c940198771659376604e62
Doran <94618170+VNTDoran@users.noreply.github.com>
2023-08-24 22:09:22 +0100

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET['command']) && $_GET['command'] === '!age') {
-    $randomAge = rand(18, 99);
-        header('Content-Type: application/json');
-    echo json_encode(['response' => "Your random age is $randomAge"]);
-}
-?>
